a0cf858ac49c3182a2e5c7a9c4b2c85658b9020a
Péter Puha <peterpuha@Peters-MacBook-Air.local>
2024-12-01 14:04:07 +0100

    new funcs

--- df.js
+++ df.js
@@ -4,10 +4,8 @@ function Series(data) {
 }
 
 Series.prototype.show = function() { console.table(this.data) }
-
-Series.prototype.eq = function(val)  { return new Series(this.data.map(e => e == val))}
-
-Series.prototype.map = function(f) { return new Series(this.data.map(f)) } 
+Series.prototype.eq = function(val)  { return new Series(this.data.map(e => e == val)) }
+Series.prototype.map = function(f) { return new Series(this.data.map(f)) }
 
 function Dataframe(data) {
     console.assert(Array.isArray(data), "expected array, got %s", typeof(data))
@@ -25,6 +23,27 @@ function Dataframe(data) {
     ) 
 }
 
-Dataframe.prototype.show = function() {
-    console.table(this.data, this.columns)
+function dataframeFromSource(selector, mapper) {
+    let el = document.querySelector(selector)
+    let content = el.innerHTML
+    let mapped = mapper(content)
+    return new Dataframe(JSON.parse(mapped))
+}
+
+Dataframe.prototype.show = function() { console.table(this.data, this.columns) }
+Dataframe.prototype.head = function(n) { return new Dataframe(this.data.slice(0, n)) }
+
+Dataframe.prototype.__defineGetter__ = function(name, func) {
+    this.columns.push(name)
+    return Object.prototype.__defineGetter__(name, func)
+}
+
+const zip = (arr0, arr1) => arr0.map((e, i) => [e, arr1[i]])
+
+Dataframe.prototype.filter = function(series) {
+    return new Dataframe(
+        zip(this.data, series.data)
+        .filter(ep => ep[1])
+        .map(ep => ep[0])
+    )
 }
